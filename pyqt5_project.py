1c348bbf6017e258d78a1be776135ce3842a67dd
keane-1 <149234971+keane-1@users.noreply.github.com>
2024-03-29 15:49:32 +0700

    Update pyqt5_project.py

---
+++ pyqt5_project.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtSql import QSqlDatabase
+
+app = QApplication(sys.argv)
+
+
+def mainWindow():
+
+    
+
+if __name__ == "__main__":
+    sys.exit(app.exec_())
